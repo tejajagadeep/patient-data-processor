b75057b486ef8984a005bc5180274f1147d94ab5
chiru1371 <88787922+chiru1371@users.noreply.github.com>
2022-11-29 20:36:56 +0530

    Add files via upload

--- Temp/backend/data.sql
+++ Temp/backend/data.sql
@@ -2,9 +2,30 @@ insert into User_Emp(user_name,password,role) values ('user@gmail.com', '{bcrypt
 insert into User_Emp(user_name,password,role) values ('admin@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','ADMIN');
 insert into User_Emp(user_name,password,role) values ('doctor@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','DOCTOR');
 insert into User_Emp(user_name,password,role) values ('drajaychekravarthi@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','DOCTOR');
+insert into User_Emp(user_name,password,role) values ('drvallika@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','DOCTOR');
+insert into User_Emp(user_name,password,role) values ('drchandhrakalasidagam@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','DOCTOR');
+insert into User_Emp(user_name,password,role) values ('dreswarreddy@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','DOCTOR');
+insert into User_Emp(user_name,password,role) values ('drpattabhikopparthi@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','DOCTOR');
+insert into User_Emp(user_name,password,role) values ('drcharanteja@gmail.com', '{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','DOCTOR');
+
 
 insert into Doctor(email_id,password,first_name,last_name,date_of_birth,gender,address,contact_number) 
 	values('drajaychekravarthi@gmail.com','{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm',' Chekravarthi','Dr Ajay','1988-05-25','Female','hyderabad',7894561230);
+
+insert into Doctor(email_id,password,first_name,last_name,date_of_birth,gender,address,contact_number) 
+	values('drvallika@gmail.com','{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm',' Jaddu','Dr Vallika','1991-07-15','Female','hyderabad',9959019263);
+
+insert into Doctor(email_id,password,first_name,last_name,date_of_birth,gender,address,contact_number) 
+	values('drchandhrakalasidagam@gmail.com','{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','Sidagam','Dr Chandhrakala','1985-03-20','Female','hyderabad',9359753113);
+
+insert into Doctor(email_id,password,first_name,last_name,date_of_birth,gender,address,contact_number) 
+	values('dreswarreddy@gmail.com','{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','Reddy','Dr Eswar','1981-11-28','Male','hyderabad',7799022533);
+
+insert into Doctor(email_id,password,first_name,last_name,date_of_birth,gender,address,contact_number) 
+	values('drcharanteja@gmail.com','{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','koyya','Dr Charan','1988-10-14','Male','hyderabad',8885621839);
+
+insert into Doctor(email_id,password,first_name,last_name,date_of_birth,gender,address,contact_number) 
+	values('drpattabhikopparthi@gmail.com','{bcrypt}$2a$10$CrYQ4MZGyFKcsRVdHzF.iu1lcFWHBcQx3i9faJj2I/KEwZ3ZNsflm','Kopparthi','Dr Pattabhi','1983-10-01','Male','hyderabad',9133442576);
 
 insert into patient(contact_number,address,first_name,last_name,gender,date_of_birth,marital_status,emergency_contact_number,email,blood_group,disease,date_visited,treatment_start_date,treatment_end_date,prescription,height,weight)
 	values(7894561230,'hyderabad','Rakel','Ramesh','Male','1999-05-28','single',7894561231,'ramesh@gmail.com','O+','High BP','2022-11-28','2021-11-28','2023-11-28','Diuretics','170cms','75kgs');
