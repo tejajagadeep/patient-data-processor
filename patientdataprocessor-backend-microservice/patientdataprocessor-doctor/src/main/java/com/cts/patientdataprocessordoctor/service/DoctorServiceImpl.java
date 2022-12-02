5de36712d4fbed4be384b44a3c173544fb2391c8
unknown <cogjava1810@iiht.tech>
2022-12-02 21:24:14 +0530

    PDP-backend all microservice 02-12-2022 2124

--- patientdataprocessor-backend-microservice/patientdataprocessor-doctor/src/main/java/com/cts/patientdataprocessordoctor/service/DoctorServiceImpl.java
+++ patientdataprocessor-backend-microservice/patientdataprocessor-doctor/src/main/java/com/cts/patientdataprocessordoctor/service/DoctorServiceImpl.java
@@ -1,42 +1,40 @@
 package com.cts.patientdataprocessordoctor.service;
-//package com.cts.patientdataprocessordoctor.service;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import com.cts.patientdataprocessordoctor.repository.DoctorRespository;
-//
-//@Service
-//public class DoctorServiceImpl implements DoctorService{
-//	
-//	@Autowired
-//	DoctorRespository doctorRespository;
-//	
+import org.springframework.stereotype.Service;
+
+import com.cts.patientdataprocessordoctor.model.Doctor;
+import com.cts.patientdataprocessordoctor.repository.DoctorRespository;
+
+@Service
+public class DoctorServiceImpl implements DoctorService{
+	
+	@Autowired
+	DoctorRespository doctorRespository;
+	
 //	@Autowired
 //	UserRepository userRepository;
-//	
+	
 //	@Autowired
 //	PasswordEncoder passwordEncoder;
-//	
-//	BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-//
-//	@Override
-//	public List<Doctor> retrieveAllDoctorDetails() {
-//		return doctorRespository.findAll();
-//	}
-//
-//
-//	@Override
-//	public Doctor retrieveDoctorDetailsById(String emailId) {
-//		return doctorRespository.findByEmailId(emailId);
-//	}
-//	
-//	@Override
-//	public Doctor doctorRegistration(Doctor doctor) {
+	
+	@Override
+	public List<Doctor> retrieveAllDoctorDetails() {
+		return doctorRespository.findAll();
+	}
+
+
+	@Override
+	public Doctor retrieveDoctorDetailsById(String emailId) {
+		return doctorRespository.findByEmailId(emailId);
+	}
+	
+	@Override
+	public Doctor doctorRegistration(Doctor doctor) {
 //		String encryptedPassword = passwordEncoder.encode(doctor.getPassword());
 //		doctor.setPassword(encryptedPassword);
 //		User user = new User();
@@ -44,9 +42,9 @@ package com.cts.patientdataprocessordoctor.service;
 //		user.setPassword(encryptedPassword);
 //		user.setRole("DOCTOR");
 //		userRepository.save(user);
-//		return doctorRespository.save(doctor);
-//	}
-//
-//
-//}
-//
+		return doctorRespository.save(doctor);
+	}
+
+
+}
+
