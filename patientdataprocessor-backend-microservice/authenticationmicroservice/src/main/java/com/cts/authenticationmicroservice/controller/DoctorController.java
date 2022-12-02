54baad26a54eaef556ee7329535d3842db7db4b5
unknown <cogjava1810@iiht.tech>
2022-12-02 21:35:21 +0530

    PDP-backend all microservice 02-12-2022 2124

--- patientdataprocessor-backend-microservice/authenticationmicroservice/src/main/java/com/cts/authenticationmicroservice/controller/DoctorController.java
+++ patientdataprocessor-backend-microservice/authenticationmicroservice/src/main/java/com/cts/authenticationmicroservice/controller/DoctorController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cts.authenticationmicroservice.model.Doctor;
+import com.cts.authenticationmicroservice.model.UserRole;
 import com.cts.authenticationmicroservice.proxy.DoctorProxy;
+import com.cts.authenticationmicroservice.repository.UserRoleRepository;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -24,6 +27,12 @@ public class DoctorController {
 
 	@Autowired
 	DoctorProxy doctorProxy;
+	
+	@Autowired
+	UserRoleRepository userRoleRepository;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
 	
 	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_DOCTOR')")
 	@GetMapping("/retrieveAllDoctorDetails")
@@ -40,6 +49,15 @@ public class DoctorController {
 	@PreAuthorize("hasRole('ROLE_ADMIN')")
 	@PostMapping("/doctorRegistration")
 	public ResponseEntity<Doctor> doctorRegistration(@RequestBody Doctor doctor){
+		
+		String encryptedPassword = passwordEncoder.encode(doctor.getPassword());
+		doctor.setPassword(encryptedPassword);
+		UserRole user = new UserRole();
+		user.setUserName(doctor.getEmailId());
+		user.setPassword(encryptedPassword);
+		user.setRole("DOCTOR");
+		userRoleRepository.save(user);
+		
 		return this.doctorProxy.doctorRegistration(doctor);
 	}
 }
