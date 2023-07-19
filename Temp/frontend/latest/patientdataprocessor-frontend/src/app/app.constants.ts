8d90486a67684364b4020191d019922c8fd203d4
tejajagadeep <62947554+tejajagadeep@users.noreply.github.com>
2023-07-19 15:34:36 +0530

    1505 19-07-2023

--- Temp/frontend/latest/patientdataprocessor-frontend/src/app/app.constants.ts
+++ Temp/frontend/latest/patientdataprocessor-frontend/src/app/app.constants.ts
@@ -1,7 +1,13 @@
-export const API_URL = "http://localhost:8081/patient-data-processor"
-export const USER_API_URL = "http://localhost:8081/patient-data-processor/api/v1.0/userrole"
-export const DOCTOR_API_URL = "http://localhost:8081/patient-data-processor/api/v1.0/doctor"
-export const PATIENT_API_URL = "http://localhost:8081/patient-data-processor/api/v1.0/patient"
-export const REPORT_API_URL = "http://localhost:8081/patient-data-processor/api/v1.0/report"
-export const RESULTS_API_URL = "http://localhost:8081/patient-data-processor/api/v1.0/results"
-export const PRESCRIPTION_API_URL = "http://localhost:8081/patient-data-processor/api/v1.0/prescription"
+export const API_URL = 'http://localhost:8083/patient-data-processor';
+export const USER_API_URL =
+  'http://localhost:8083/patient-data-processor/api/v1.0/userrole';
+export const DOCTOR_API_URL =
+  'http://localhost:8083/patient-data-processor/api/v1.0/doctor';
+export const PATIENT_API_URL =
+  'http://localhost:8083/patient-data-processor/api/v1.0/patient';
+export const REPORT_API_URL =
+  'http://localhost:8083/patient-data-processor/api/v1.0/report';
+export const RESULTS_API_URL =
+  'http://localhost:8083/patient-data-processor/api/v1.0/results';
+export const PRESCRIPTION_API_URL =
+  'http://localhost:8083/patient-data-processor/api/v1.0/prescription';
